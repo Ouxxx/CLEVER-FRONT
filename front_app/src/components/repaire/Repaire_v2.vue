9fe940ed50bbed8531215139aa092699b0615b19
GalactusLeMinus <workitnowu@gmail.com>
2020-07-08 18:42:20 +0200

    ajout de infopoints - en cours de travaux

--- front_app/src/components/repaire/Repaire_v2.vue
+++ front_app/src/components/repaire/Repaire_v2.vue
@@ -12,12 +12,25 @@
                     <p class="repaire-avatar-register"> 
                         {{$store.getters.getId}} </p>
                 </div>
+                <div class="infopoints-repaire">
+                    <img class="infopoints-repaire-img" src="../../assets/img/infopoints-repaire.png" alt="infopoints">
+                </div>
+
             </div>
             <div class="introduce-yourself">
+                <div class="infopoints-repaire">
+                    <img class="infopoints-repaire-img" src="../../assets/img/infopoints-repaire.png" alt="infopoints">
+                </div>
             </div>
             <div class="thread-repaire">
+                <div class="infopoints-repaire">
+                    <img class="infopoints-repaire-img" src="../../assets/img/infopoints-repaire.png" alt="infopoints">
+                </div>
             </div>
             <div class="hero-contents">
+                <div class="infopoints-repaire">
+                    <img class="infopoints-repaire-img" src="../../assets/img/infopoints-repaire.png" alt="infopoints">
+                </div>
             </div>
         </div>
 
@@ -49,13 +62,14 @@ export default {
     
 }
 
+
+
+/*TO DO: User devra pouvoir personnaliser sa banniere par un chgt de couleur ou une image apportée*/
 .banner {
-    
     width: 1100px;
     border-color: aliceblue;
     border-style: dotted;
     border-width: 1px;
-    
     margin-left: 50px;
     padding-top: 10px;
     padding-bottom: 10px;
@@ -64,14 +78,21 @@ export default {
 }
 
 .repaire-avatar{
+    display: inline-block;
+    vertical-align: middle;
     width: 150px;
     margin-bottom: 15px;
+    
+}
+
+.infopoints-repaire{
+    float: right;  
 }
 
 .repaire-avatar-img{
     display: block;
     bottom: 15px;
-    margin-right:auto;
+    margin-right: auto;
     margin-left: auto;
 }
 
@@ -79,13 +100,22 @@ export default {
     display: block;
     text-align: center;
     font-size: 22px;
-    color : rgb(220,183,102);
+    color : rgb(255, 255, 255);
     margin: 0;
 }
 
+.infopoints-repaire{
+    display: inline-block;
+    border-color: aliceblue;
+    border-style: outset;
+    border-width: 1px;
+
+}
+
+/*TO DO: User devra pouvoir personnaliser par écrit*/
 .introduce-yourself{
     
-    display: inline-block;
+    display: block;
     width: 1100px;
     border-color: aliceblue;
     border-style: dotted;
@@ -97,9 +127,9 @@ export default {
     min-height: 150px;
     min-width: 250px;
 }
-
+/*TO DO: actualisation de la page avec des dernières actualités. Actualités = capacité d'interagir avec les gens qui regardent/suivent en mettant des posts.*/
 .thread-repaire{
-    display: inline-block;
+    display: block;
     width: 1100px;
     background-color: aliceblue;
     margin-top: 10px;
@@ -109,15 +139,16 @@ export default {
     min-height: 150px;
     min-width: 250px;
 }
-
+/*TO DO: Actualisation du contenu du Hero*/
 .hero-contents{
-    display: inline-block;
+    display: block;
     width: 1100px;
+    height: 500px;
     background-color: aliceblue;
+    padding-top: 10px;
+    padding-bottom: 10px;
     margin-top: 10px;
     margin-left: 50px;
-    padding-right: 1100px;
-    padding-bottom: 10px;
     min-height: 150px;
     min-width: 250px;
 }
