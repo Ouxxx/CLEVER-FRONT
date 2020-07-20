687b7332bbaf4a8f2c880cdebe720990e868e864
Oux <oux.cleverhero@gmail.com>
2020-07-20 21:48:49 +0200

    creation composant : notification_v1 (pas fini)

--- front_app/src/components/Home_v1.vue
+++ front_app/src/components/Home_v1.vue
@@ -3,7 +3,12 @@
         
         <header-ch></header-ch>
 
+
         <sidebar-ch></sidebar-ch>
+        <notification message="Cool c'est OK" isErr="false"></notification>
+        <notification message="Cool c'est KO" isErr="true"></notification>
+        <notification message="Cool je sais pas" ></notification>
+
 
         <div class="home-main">
             <element-list category="En cours de visionnage"></element-list>
@@ -19,9 +24,10 @@
 import headerCh from './blocs/header_v3'
 import sidebarCh from './blocs/sidebar_v1'
 import elementList from './blocs/list_elements_v1'
+import notification from './blocs/notification_v1'
 
 export default {
-    components: { headerCh, sidebarCh, elementList },
+    components: { headerCh, sidebarCh, elementList, notification },
     data () {
         return {}
     }
---
+++ front_app/src/components/blocs/notification_v1.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="notification-root" :class="[ err == null || err ? 'notification-root-error' : '']">
+    <p class="notification-p" :class="[ err == null || err ? 'notification-p-error' : '']">
+        {{ err == null || err + ':' + message }}
+    </p>
+</div>    
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+            required: true
+        },
+        isErr:{
+            type: String,
+            required: false
+        }
+    },
+
+    computed : {
+        err: function () {
+            return !(this.isErr == false)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.notification-root {
+    margin: 10px 60px 0;
+    padding: 0 15px;
+    border-style: solid;
+    border-radius: 7px;
+    border-color: rgb(0,139,139); 
+    border-width: 2px;
+    background-color: rgb(0,139,139,0.2); 
+}
+
+.notification-root-error{
+    border-color: red;
+    background-color: rgb(255,0,0,0.2);
+}
+
+.notification-p {
+    color: rgb(0,139,139);
+    margin: 5px
+}
+
+.notification-p-error {
+    color: red;
+}
+
+
+</style>
