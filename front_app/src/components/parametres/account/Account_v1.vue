dd7cb1a74f9aaa28a8be12cfa672992fcf115104
Oux <oux.cleverhero@gmail.com>
2020-09-21 19:08:25 +0200

    Page Compte ok

--- front_app/src/components/parametres/account/Account_v1.vue
+++ front_app/src/components/parametres/account/Account_v1.vue
@@ -1,5 +1,43 @@
 <template>
     <div class="account-root">
+        <table>
+            <tr>
+                <td><p>Matricule :</p></td>
+                <td><p>{{$store.getters.getId}}</p></td>
+            </tr>
+            <tr>
+                <td><p>Nom :</p></td>
+                <td><p>Kokoro</p></td>
+            </tr>
+            <tr>
+                <td><p>Prénom :</p></td>
+                <td><p>Sukiru Kokoro</p></td>
+            </tr>
+            <tr>
+                <td><p>Mot de passe :</p></td>
+                <td><p>**********</p></td>
+                <td><p class="clickable" @click="changePassword()">Modifier mon mot de passe</p></td>
+            </tr>
+            <tr>
+                <td><p>E-mail :</p></td>
+                <td><p>laplusbelle@suki.fr</p></td>
+                <td><p class="clickable" @click="changeEmail()">Modifier mon adresse e-mail</p></td>
+            </tr>
+            <tr>
+                <td><p>Téléphone :</p></td>
+                <td><p>0600000000</p></td>
+                <td><p class="clickable" @click="changePhone()">Modifier mon numéro de télephone</p></td>
+            </tr>
+            <tr>
+                <td><p>Membre depuis :</p></td>
+                <td><p>11/09/2020</p></td>
+            </tr>
+            <tr>
+                <td></td><td></td>
+                <td><p class="clickable" @click="changePhone()">Supprimer mon compte</p></td>
+            </tr>
+        </table>
+
 
     </div>
 </template>
@@ -7,6 +45,17 @@
 <script>
 
 export default {
+    methods : {
+        changePassword () {
+            console.log("Vous tentez de changeer votre mot de passe.")
+        },
+        changeEmail () {
+            console.log("Vous tentez de changeer votre Email.")
+        },
+        changePhone () {
+            console.log("Vous tentez de changeer votre numéro de télephone.")
+        }
+    }
 }
 </script>
 
@@ -26,6 +75,17 @@ export default {
     }    
 }
 
+.account-root table{
+    margin: auto
+}
+
+.account-root p {
+    color: white;
+    margin: 10px 50px ;
+}
+.clickable {
+    text-decoration: underline;
+}
 
 
 </style>
