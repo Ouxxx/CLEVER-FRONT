2e924e378fb0995ca76d44396201744e3ccdae28
GalactusLeMinus <workitnowu@gmail.com>
2020-10-05 17:32:58 +0200

    rectification message creation compte/ user confirmation

--- front_app/src/components/create_user/User_confirmation_v1.vue
+++ front_app/src/components/create_user/User_confirmation_v1.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="form-data">
         <p class="form-p">Votre compte a bien été créé, félicitations {{$store.getters.getId}} ! 
-            "Ton matricule, précédemment cité, sera ton identité de Héro. Il est unique alors fais-en bon usage." 
+            "Le matricule précédemment cité sera votre identité de Héro. Il est unique alors faîtes-en bon usage." 
         </p>
     </div>
 </template>
