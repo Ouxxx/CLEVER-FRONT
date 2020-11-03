e4fb67198299c740b95b840240990457ea8dca58
GalactusLeMinus <workitnowu@gmail.com>
2020-11-03 19:03:41 +0100

    ajout base posée

--- front_app/src/components/Ajout/ajout_v2.vue
+++ front_app/src/components/Ajout/ajout_v2.vue
@@ -57,16 +57,16 @@
                 </div>
                 <div class="format">
                     <p>Quel est son support principal ?</p>
-                    <label class="media-container">Vidéo
-                        <input type="radio" v-model="y" value="video" name="radio" checked>
+                    <label class="add-container">Vidéo
+                        <input type="radio" v-model="y" value="video" name="radio2" checked>
                         <span class="class"></span>
                     </label>
-                    <label class="media-container">Ecrit
-                        <input type="radio" v-model="y" value="writing" name="radio">
+                    <label class="add-container">Ecrit
+                        <input type="radio" v-model="y" value="writing" name="radio2">
                         <span class="class"></span>
                     </label>
-                    <label class="media-container">Image
-                        <input type="radio" v-model="y" value="image" name="radio">
+                    <label class="add-container">Image
+                        <input type="radio" v-model="y" value="image" name="radio2">
                         <span class="class"></span>
                     </label>
                 </div> 
@@ -95,13 +95,13 @@
                     </select>
                 </div>
                 <div class="format">
-                    <p>Quel est son support principal ?</p>
-                    <label class="media-container">Ecrit
-                        <input type="radio" v-model="y" value="writing" name="radio" checked>
+                    <p class="title">Quel est son support principal ?</p>
+                    <label class="add-container">Ecrit
+                        <input type="radio" v-model="y" value="writing" name="radio3" checked>
                         <span class="class"></span>
                     </label>
-                    <label class="media-container">Image
-                        <input type="radio" v-model="y" value="image" name="radio">
+                    <label class="add-container">Image
+                        <input type="radio" v-model="y" value="image" name="radio3">
                         <span class="class"></span>
                     </label>
                 </div>
@@ -109,23 +109,85 @@
         <div class="menu-middle1" v-if="y === 'video'">
             <div class="column-container">
                 <div class="column">
-                    <tr>Importer une image de présentation (optionnel)</tr>
-                    <tr>Titre de la catégorie :<input type="text" required style="width:100%"></tr>
+                    <tr class="column-import">Importer une image de présentation (optionnel)
+                        <file-selector max="1" v-model="files">
+                            <input type="file"/>
+                        </file-selector>
+                    </tr>
+                    <tr>Titre de la catégorie :<input class="input-title" type="text" required></tr>
                     <tr>Description de la catégorie :</tr>
                     <textarea name="input-desc" value="text-desc" required style="width:100%; height:200px">
                     </textarea>
                 </div>
                 <div class="column">
                     <tr>Importer une image de présentation (optionnel)</tr>
-                    <tr>Importer une image de présentation (optionnel)</tr>
                 </div>
             </div>
+            <div class="price">Prix
+                <input type="price-inputted" required style="width:100px">
+                <label class="checkbox">Je souhaite que mon contenu soit gratuit.
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+            <div class="options1">
+            <p class="title">Facultatif </p>
+            <div class="extra">
+                <div class="top-part">
+                    <label class="checkbox">Ajouter un support secondaire :
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                    <div class="format-extra">
+                            <label class="format-extra-writing">Ecrit
+                                <input type="radio" value="writing-extra" name="radio" checked>
+                                <span class="class"></span>
+                            </label>
+                            <label class="format-extra-image">Image
+                                <input type="radio" value="image-extra" name="radio">
+                                <span class="class"></span>
+                            </label>
+                            <table class="file">
+                                <tr class="file-import">Importer un fichier
+                                    <file-selector max="5" v-model="files">
+                                        <input type="file"/>
+                                    </file-selector>
+                                </tr>
+                                <tr class="file-time">Temps de placement</tr>
+                                <button class="file-add" @click="addFile()">Ajouter +</button>
+                            </table>
+                    </div>
+                </div>
+                <div class="bot-part">
+                    <label class="checkbox">Planification :
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                    <div class="scheduler">
+                        <select>
+                            <option value="0">Scheduler : A voir comment faire</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="page-validation">
+                <label class="checkbox">Je certifie avoir respecté les conditions générales de vente et d'utilisation
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+                <button class="validation-btn">JE VALIDE</button>
+            </div>
+        </div>
         </div>
         
         <div class="menu-middle2" v-if="y === 'writing'">
             <div class="column-container">
                 <div class="column">
-                    <tr>Importer une image de présentation (optionnel)</tr>
+                    <tr class="column-import">Importer une image de présentation (optionnel)
+                        <file-selector max="5" v-model="files">
+                            <input type="file"/>
+                        </file-selector>
+                    </tr>
                     <tr>Titre :<input type="text" required style="width:100%"></tr>
                     <tr>Résumé :</tr>
                     <textarea name="input-desc" value="text-desc" required style="width:100%; height:200px">
@@ -137,12 +199,82 @@
                     </textarea>
                 </div>
             </div>
+            <div class="price">Prix
+                <input type="price-inputted" required style="width:100px">
+                <label class="checkbox">Je souhaite que mon contenu soit gratuit.
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+            <div class="options2">
+                <p class="title">Facultatif </p>
+                <div class="extra">
+                    <div class="top-part">
+                        <div class="ed-book">
+                            <label class="checkbox">ED-Book :
+                                <input type="checkbox">
+                                <span class="checkmark"></span>
+                            </label>
+                            <table class="file">
+                                <tr class="file-import">Importer un fichier
+                                    <file-selector max="5" v-model="files">
+                                        <input type="file"/>
+                                    </file-selector>
+                                </tr>
+                                <tr class="file-time">Temps de placement</tr>
+                                <button class="file-add" @click="addFile()">Ajouter +</button>
+                            </table>
+                        </div>
+                        <label class="checkbox">Ajouter un support secondaire :
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                        <div class="format-extra">
+                            <label class="format-extra-image">Image
+                                <input type="radio" value="image-extra" name="radio" checked>
+                                <span class="class"></span>
+                            </label>
+                            <table class="file">
+                                <tr class="file-import">Importer un fichier
+                                    <file-selector max="5" v-model="files">
+                                        <input type="file"/>
+                                    </file-selector>
+                                </tr>
+                                <tr class="file-time">Temps de placement</tr>
+                                <button class="file-add" @click="addFile()">Ajouter +</button>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="bot-part">
+                        <label class="checkbox">Planification :
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                        <div class="scheduler">
+                            <select>
+                                <option value="0">Scheduler : A voir comment faire</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="page-validation">
+                    <label class="checkbox">Je certifie avoir respecté les conditions générales de vente et d'utilisation
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                    <button class="validation-btn">JE VALIDE</button>
+                </div>
+            </div>
         </div>
 
         <div class="menu-middle3" v-if="y === 'image'">
             <div class="column-container">
                 <div class="column">
-                    <tr>Importer une image de présentation (optionnel)</tr>
+                    <tr class="column-import">Importer une image de présentation (optionnel)
+                        <file-selector max="5" v-model="files">
+                            <input type="file"/>
+                        </file-selector>
+                    </tr>
                     <tr>Titre :<input type="text" required style="width:100%"></tr>
                     <tr>Résumé :</tr>
                     <textarea name="input-desc" value="text-desc" required style="width:100%; height:200px">
@@ -153,14 +285,72 @@
                 </div>
             </div>
             <div class="price">Prix
-                    <input type="price-inputted" required style="width:100px">
-                    <label class="free">Je souhaite que mon contenu soit gratuit.
+                <input type="price-inputted" required style="width:100px">
+                <label class="checkbox">Je souhaite que mon contenu soit gratuit.
+                    <input type="checkbox">
+                    <span class="checkmark"></span>
+                </label>
+            </div>
+            <div class="options3">
+                <p class="title">Facultatif </p>
+                <div class="extra">
+                    <div class="top-part">
+                        <div class="ed-book">
+                            <label class="checkbox">ED-Book :
+                                <input type="checkbox">
+                                <span class="checkmark"></span>
+                            </label>
+                            <table class="file">
+                                <tr class="file-import">Importer un fichier
+                                    <file-selector max="5" v-model="files">
+                                        <input type="file"/>
+                                    </file-selector>
+                                </tr>
+                                <tr class="file-time">Temps de placement</tr>
+                                <button class="file-add" @click="addFile()">Ajouter +</button>
+                            </table>
+                        </div>
+                        <label class="checkbox">Ajouter un support secondaire :
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                        <div class="format-extra">
+                            <label class="format-extra-writing">Ecrit
+                                <input type="radio" value="writing-extra" name="radio" checked>
+                                <span class="class"></span>
+                            </label>
+                            <table class="file">
+                                <tr class="file-import">Importer un fichier
+                                    <file-selector max="5" v-model="files">
+                                        <input type="file"/>
+                                    </file-selector>
+                                </tr>
+                                <tr class="file-time">Temps de placement</tr>
+                                <button class="file-add" @click="addFile()">Ajouter +</button>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="bot-part">
+                        <label class="checkbox">Planification :
+                            <input type="checkbox">
+                            <span class="checkmark"></span>
+                        </label>
+                        <div class="scheduler">
+                            <select>
+                                <option value="0">Scheduler : A voir comment faire</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="page-validation">
+                    <label class="checkbox">Je certifie avoir respecté les conditions générales de vente et d'utilisation
                         <input type="checkbox">
                         <span class="checkmark"></span>
                     </label>
+                    <button class="validation-btn">JE VALIDE</button>
+                </div>
             </div>
-        </div>
-
+        </div>        
     </div>
 </div>
 </template>
@@ -174,6 +364,12 @@ export default {
             x: 'one',
             y: 'video',
         }
+    },
+    computed: {
+    addFile() {
+        return {
+        }
+    }
     }
 }
 </script>
@@ -214,6 +410,7 @@ export default {
     display: table;
     width: 90%;
     background-color: rgb(17, 26, 41); 
+    margin-top: 25px;
 }
 
 .column {
@@ -224,13 +421,17 @@ export default {
     border-color:rgba(168, 135, 75, 0.6);
 }
 
+.input-title {
+    width:100%;
+}
+
 /* Prix */
 
 .price {
     color: white;
 }
 
-.free {
+.checkbox {
     display: inline-block;
     position: relative;
     color: rgba(168, 135, 75, 0.6);
@@ -245,7 +446,7 @@ export default {
     user-select: none;
 }
 
-.free input {
+.checkbox input {
     position: absolute;
     opacity: 0;
     cursor: pointer;
@@ -263,11 +464,11 @@ export default {
     background-color: #eee;
 }
 
-.free:hover input ~ .checkmark {
+.checkbox:hover input ~ .checkmark {
     background-color: #ccc;
 }
 
-.free input:checked ~ .checkmark {
+.checkbox input:checked ~ .checkmark {
     background-color: rgba(168, 135, 75, 0.6);
 }
 
@@ -277,11 +478,11 @@ export default {
     display: none;
 }
 
-.free input:checked ~ .checkmark:after {
+.checkbox input:checked ~ .checkmark:after {
     display: block;
 }
 
-.free .checkmark:after {
+.checkbox .checkmark:after {
     left: 7px;
     top: 3px;
     width: 4px;
@@ -291,5 +492,52 @@ export default {
     -webkit-transform: rotate(45deg);
     -ms-transform: rotate(45deg);
     transform: rotate(45deg);
+}
+
+/* Options */
+
+.extra {
+    color: white;
+    margin-left: 80px;
+}
+
+.title {
+    margin-left: 50px;
+    font-size: large;
+}
+
+.format-extra {
+    display: inline;
+    margin-left: 30px;
+}
+
+.ed-book {
+    display: block;
+}
+
+.file {
+    background-color: white;
+    color: grey;
+    margin-left: 40px;
+    margin-top: 10px;
+}
+
+.bot-part {
+    margin-top: 20px;
+}
+
+.scheduler {
+    display: inline;
+    margin-left: 40px;
+}
+
+.validation-btn {
+    display: block;
+    margin-top: 50px;
+}
+
+.page-validation {
+    margin-top: 50px;
+    margin-left: 50px;
 }
 </style>
