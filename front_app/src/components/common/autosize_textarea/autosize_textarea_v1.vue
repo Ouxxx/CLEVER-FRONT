5d8bba1cdc10478a3275a3be88006812555ca129
GalactusLeMinus <workitnowu@gmail.com>
2020-11-18 16:51:20 +0100

    en cours : elaboration textarea autosize / repaire

---
+++ front_app/src/components/common/autosize_textarea/autosize_textarea_v1.vue
@@ -0,0 +1,45 @@
+<template>
+    <textarea 
+        class="textarea" 
+        :name="name" 
+        @input="$emit('input', $event.target.value); resize($event)" 
+        :id="name" 
+        :value="value">
+    </textarea>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            required: true,
+            type: String,
+        },
+        value: {
+            required: false, 
+        }
+    },
+    methods: {
+        resize (a) {
+            a.target.style.height = 'auto'
+            a.target.style.height = '${a.target.scrollHeight}px'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.textarea {
+    display: inline;
+    width: 90%;
+    resize: none;
+    font-size: inherit;
+    outline: none;
+    padding: 20px;
+    min-height: 40px;
+    box-sizing: border-box;
+}
+
+
+</style>
--- front_app/src/components/repaire/gerer_mon_profil/profile_management_v1.vue
+++ front_app/src/components/repaire/gerer_mon_profil/profile_management_v1.vue
@@ -7,15 +7,12 @@
         <div class="profile-main">
             <div class="introduce-profile content-block-property">
                 <div class="flexbox">
-                        <textarea class="input-text" placeholder="Introduisez-vous et votre contenu"
-                        rows="1" cols="40" :input="autoSizeTextArea(this)">
-                            Coucou les loulous !
-                            Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines
-                            dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un 
-                            professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, 
-                            consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature
-                            classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.
-                        </textarea>
+                        <autosizeTextarea
+                            name="text"
+                            v-model="form.text"/>
+
+                            {{ form }}
+
                         <div class="logo-bar">
                             <img class="instagram logo" src="@/assets/img/instagram.png" alt="instagram">
                             <img class="linkedin logo" src="@/assets/img/linkedin.png" alt="linkedin">
@@ -30,10 +27,16 @@
 
 <script>
 import banner from '../../common/banner/Banner_v1'
+import autosizeTextarea from '../../common/autosize_textarea/autosize_textarea_v1'
+
 export default {
-    components : {banner},
-    methods: {
-        
+    components : {banner, autosizeTextarea},
+    data () {
+        return {
+            form: {
+                body: 'aze'
+            }
+        }
     }
 }
     
@@ -55,13 +58,9 @@ export default {
 
 /*introduce*/
 
-.input-text {
-    display: inline;
-    width: 90%;
-    resize: none;
-    overflow: visible;
-    min-height: 40px;
-    max-height: 100px;
+.flexbox {
+    max-width: 900px;
+    margin: 0 auto;
 }
 
 .logo-bar {
