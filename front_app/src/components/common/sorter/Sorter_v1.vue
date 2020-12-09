1fb18c55dc0fa6b5edf5ddebc2ea9edf602048e3
Oux <oux.cleverhero@gmail.com>
2020-12-09 22:54:08 +0100

    composant picker et filter

---
+++ front_app/src/components/common/sorter/Sorter_v1.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="sorter-root">
+        <div class="sorter-button" @click="openClose()">
+            <div class="sorter-button-value"><p>{{value}}</p></div>
+            <div class="sorter-button-cross"><p>{{cross}}</p></div>
+        </div>
+        
+        <div v-show="isOpen" class="sorter-choices">
+            <div class="sorter-choice" :class="{'on-hover' : isHover === 'date', 'selected' : selected === 'Date' }"
+                    @click="choose('Date')" @mouseover="setHover('date')" @mouseout="setHover('')"><p>Date</p></div>
+            <div class="sorter-choice" :class="{'on-hover' : isHover === 'matricule', 'selected' : selected === 'Matricule' }"
+                    @click="choose('Matricule')" @mouseover="setHover('matricule')" @mouseout="setHover('')"><p>Matricule</p></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        selected: {
+            type: String,
+            required: true
+        }
+    },
+    data () {
+        return {
+            value : "Trier par",
+            isOpen : false,
+            isHover : "",
+            cross : '>'
+        }
+    },
+    methods: {
+        choose (value) {
+            this.value = value
+            this.$emit('sorter', value)
+            this.openClose()
+        },
+        openClose () {
+            this.isOpen = !this.isOpen
+        },
+        setHover(elem) {
+            this.isHover = elem
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.sorter-root {
+    position: relative;
+    cursor: pointer;
+    height: 40px;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+}
+.sorter-root p {
+    line-height: 2.5em;
+    margin: 0;
+}
+.sorter-button {
+    background-color: rgba(218,165, 32, 1);
+}
+.sorter-choices {
+    position: absolute;
+    width: 100%;
+    border-top-style: solid;
+    border-width: 1px;
+    z-index: 2020;
+}
+
+.sorter-button-cross {
+    display: inline-block;
+    line-height: 2.5em;
+    text-align: center;
+    height: 40px;
+    width: 40px;
+    transform: rotate(90deg);
+    font-weight: bold;
+}
+.sorter-button-value{
+    display: inline-block;
+    padding: 0 10px;
+    min-width: 90px;
+
+
+}
+.sorter-choice {
+    background-color: rgba(218,165, 32, 1);
+    height: 40px;
+    padding: 0 10px;
+}
+
+.on-hover {
+    background-color: rgba(218,165, 32, 0.6);
+}
+.selected {
+    background-color: rgba(105, 68, 12, 0.6);
+}
+
+
+</style>
+
--- front_app/src/components/repaire/media/media_v1.vue
+++ front_app/src/components/repaire/media/media_v1.vue
@@ -1,145 +1,27 @@
 <template>
-    <div class='media-root' style="background-color: blue"> 
-            <chPicker />
-        
-        <div class= "media-functions">
-            
-            <div class="search-bar">
-                <input type="text" name="search" placeholder="Rechercher un contenu plus rapidement...">
+    <div class='media-root'> 
+
+            <div class="media-function">
+                <ch-picker @picker-selected="changeDocuments" @sorter="changeSort" :selected="this.docType" />
             </div>
-                
-            <div class="media-sort">
 
-                
-                
-                <div class="media-btn" @click="showVideos()">Vidéo</div>
-                <div class="media-btn" @click="showEcrits()">Ecrit</div>
-                <div class="media-btn" @click="showImages()">Image</div>
-                <div class="dropdown">
-                    <div class="media-btn media-btn-separator" >
-                        <img class="triangle-dropdown" src="../../../assets/rendus/MonRepaire/a_ObjetsCommuns/triangle_dropdownmenu_v2.png"
-                                alt="triangle">
-                    </div>
-                    <div class="dropdown-content">
-                        <div class="#">Date</div>
-                        <div class="#">Matricule</div>
-                    </div>
-                </div>
-            </div>  
-            
-        </div>
-
-        <div class="media-main" style="background-color : yellow">
-            <ch-mosaique :contents="videos" contentType="video" />
-        </div>
-
+            <div class="media-main" style="background-color : yellow">
+                <ch-mosaique :contents="videos" contentType="video" />
+            </div>
     </div>
 </template>
 
-<style scoped>
-
-.media-root {
-    margin: auto;
-    padding-left: 50px;
-}
-@media screen and (max-width: 1200px){
-    .media-root {
-        width: 80%;
-    }    
-}
-@media screen and (min-width: 1000px){
-    .media-root {
-        width: 1000px auto;
-    }    
-}
-.triangle-dropdown {
-    height: 40%;
-    width: 40%;
-}
-.media-functions {
-    display: flex;
-    position: relative;
-    align-items: baseline;
-}
-.search-bar {
-    flex: 1 1 auto;
-    text-align: right;
-}
-.search-bar input {
-    cursor: pointer;
-    text-align: center;
-    height: 40px;
-    width: 280px;
-    border-radius: 5px;
-    -webkit-transition: width 0.4s ease-in-out;
-    transition: width 0.4s ease-in-out;
-}
-.search-bar input:focus {
-    width: 60%;
-}
-.media-sort {
-    display: inline-block;
-    margin: 0 180px 20px 10px;
-    padding-top: 20px;
-    cursor: pointer;
-}
-.media-btn {
-    display: inline-block;
-    background-color: rgba(218,165, 32, 1);
-    color: white;    
-    padding: 12px;
-    cursor: pointer;
-}
-.media-btn-separator {
-    border-left:1px solid navy;
-}
-
-.dropdown {
-    display: inline-block;
-    position: absolute;
-  
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color:rgba(218,165, 32, 1);
-    min-width: 160px;
-    z-index: 1;
-}
-
-.dropdown-content div {
-    display: block;
-    color: white;
-    padding: 12px 16px;
-    text-decoration: none;
-  
-}
-
-.dropdown-content div:hover {
-    background-color: rgba(105, 68, 12, 0.6);
-    color: white;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-.btn:hover {
-  background-color: rgba(218,165, 32, 0.6);
-}
-
-</style>
-
 <script>
 import chMosaique from '../../common/mosaique/Mosaique_v1'
-import chPicker from '../../common/content_picker/Content_picker_v1'
+import chPicker from '../../common/content_picker/Picker_v2'
 
 export default {
     components: { chMosaique, chPicker },
     data () {
  
         return {
+            docType : 'video',
+            sortType : '',
             images : [],
             ecrits : [],
             videos : [
@@ -204,16 +86,108 @@ export default {
         }
     },
     methods: {
-        showEcrits : function () {
-
+        changeDocuments : function (docType) {
+            this.docType = docType
         },
-        showImages : function () {
-            
-        },
-        showVideos : function () {
-            
+        changeSort : function (sortType) {
+            this.sortType = sortType
         }
-
     }
 }
 </script>
+
+<style scoped>
+
+.media-root {
+    margin: auto;
+    padding-left: 50px;
+}
+@media screen and (max-width: 1200px){
+    .media-root {
+        width: 80%;
+    }    
+}
+@media screen and (min-width: 1000px){
+    .media-root {
+        width: 1000px auto;
+    }    
+}
+
+.media-function{
+    padding-right: 0;
+}
+.triangle-dropdown {
+    height: 40%;
+    width: 40%;
+}
+.media-functions {
+    display: flex;
+    position: relative;
+    align-items: baseline;
+}
+.search-bar {
+    flex: 1 1 auto;
+    text-align: right;
+}
+.search-bar input {
+    cursor: pointer;
+    text-align: center;
+    height: 40px;
+    width: 280px;
+    border-radius: 5px;
+    -webkit-transition: width 0.4s ease-in-out;
+    transition: width 0.4s ease-in-out;
+}
+.search-bar input:focus {
+    width: 60%;
+}
+.media-sort {
+    display: inline-block;
+    margin: 0 180px 20px 10px;
+    padding-top: 20px;
+    cursor: pointer;
+}
+.media-btn {
+    display: inline-block;
+    background-color: rgba(218,165, 32, 1);
+    color: white;    
+    padding: 12px;
+    cursor: pointer;
+}
+.media-btn-separator {
+    border-left:1px solid navy;
+}
+
+.dropdown {
+    display: inline-block;
+    position: absolute;
+  
+}
+
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color:red;
+    min-width: 160px;
+    z-index: 1;
+}
+
+.dropdown-content div {
+    display: block;
+    color: white;
+    padding: 12px 16px;
+    text-decoration: none;
+  
+}
+
+.dropdown-content div:hover {
+    background-color: rgba(105, 68, 12, 0.6);
+    color: white;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+</style>
+
