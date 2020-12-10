222e688d0ab18c7e75dca8515317018b31645dc2
Oux <oux.cleverhero@gmail.com>
2020-12-10 20:10:14 +0100

    finition repaire/mediatheque

--- front_app/src/components/repaire/media/media_v1.vue
+++ front_app/src/components/repaire/media/media_v1.vue
@@ -5,11 +5,25 @@
                 <ch-picker @picker-selected="changeDocuments" @sorter="changeSort" :selected="this.docType" />
             </div>
 
-            <div class="media-main" style="background-color : yellow">
+            <div class="media-main" >
                 <ch-mosaique :contents="videos" contentType="video" />
             </div>
     </div>
 </template>
+
+<style scoped>
+
+.media-root {
+    padding-left: 50px;
+}
+
+.media-function{
+    padding-top: 30px;
+    padding-bottom: 50px;
+}
+
+</style>
+
 
 <script>
 import chMosaique from '../../common/mosaique/Mosaique_v1'
@@ -96,98 +110,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-.media-root {
-    margin: auto;
-    padding-left: 50px;
-}
-@media screen and (max-width: 1200px){
-    .media-root {
-        width: 80%;
-    }    
-}
-@media screen and (min-width: 1000px){
-    .media-root {
-        width: 1000px auto;
-    }    
-}
-
-.media-function{
-    padding-right: 0;
-}
-.triangle-dropdown {
-    height: 40%;
-    width: 40%;
-}
-.media-functions {
-    display: flex;
-    position: relative;
-    align-items: baseline;
-}
-.search-bar {
-    flex: 1 1 auto;
-    text-align: right;
-}
-.search-bar input {
-    cursor: pointer;
-    text-align: center;
-    height: 40px;
-    width: 280px;
-    border-radius: 5px;
-    -webkit-transition: width 0.4s ease-in-out;
-    transition: width 0.4s ease-in-out;
-}
-.search-bar input:focus {
-    width: 60%;
-}
-.media-sort {
-    display: inline-block;
-    margin: 0 180px 20px 10px;
-    padding-top: 20px;
-    cursor: pointer;
-}
-.media-btn {
-    display: inline-block;
-    background-color: rgba(218,165, 32, 1);
-    color: white;    
-    padding: 12px;
-    cursor: pointer;
-}
-.media-btn-separator {
-    border-left:1px solid navy;
-}
-
-.dropdown {
-    display: inline-block;
-    position: absolute;
-  
-}
-
-.dropdown-content {
-    display: none;
-    position: absolute;
-    background-color:red;
-    min-width: 160px;
-    z-index: 1;
-}
-
-.dropdown-content div {
-    display: block;
-    color: white;
-    padding: 12px 16px;
-    text-decoration: none;
-  
-}
-
-.dropdown-content div:hover {
-    background-color: rgba(105, 68, 12, 0.6);
-    color: white;
-}
-
-.dropdown:hover .dropdown-content {
-  display: block;
-}
-
-</style>
 
