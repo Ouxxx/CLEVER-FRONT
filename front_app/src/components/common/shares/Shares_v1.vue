391789cc27d09a3bec4617930f5981aa7af92860
Oux <oux.cleverhero@gmail.com>
2021-02-06 12:15:42 +0100

    init composant shares

---
+++ front_app/src/components/common/shares/Shares_v1.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="shares-root">
+        <modal v-if="false" ></modal>
+        <div class="shares-item">
+            <img src="@/assets/test.png" alt="">
+        </div>
+        <div class="shares-item">
+            <img src="@/assets/testp.png" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+import modal from '../modal/Modal_v1'
+export default {
+    components : { modal }
+    
+}
+</script>
+
+<style scoped>
+
+.shares-root {
+    display: inline-flex
+}
+
+.shares-item {
+    flex-grow: 1;
+    padding: 5px;
+    background-color: aqua;
+}
+
+img {
+    /* width : 40px; */
+    background-color: blueviolet
+}
+
+</style>
