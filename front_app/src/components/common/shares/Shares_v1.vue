9105e7cd9e4baac75d30882ccf760505f3e199ff
Oux <oux.cleverhero@gmail.com>
2021-02-16 21:26:22 +0100

    le composant Shares permet maintenant de supprimer des liens

--- front_app/src/components/common/shares/Shares_v1.vue
+++ front_app/src/components/common/shares/Shares_v1.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="shares-root">
         <modal v-if="isAddModalOpen" title="Ajouter un lien" submit-label="Valider" cancel-label="Annuler" :with-submit="isSubmitEnable"
-                @modal-submit="addLink" @modal-cancel="closeModal" >
-            <div>
+                @modal-submit="addLink" @modal-cancel="closeAddModal" >
+            <div class="add-modal-root">
                 <div class="modal-section">
                     <div class="modal-section-form">Choisissez le type de lien à ajouter</div>
-                    <div class="link-types modal-section-content">
-                        <div class="link-type" v-for="share in getShares" :key="share.name"
+                    <div class="modal-section-content">
+                        <div class="link-type img-container" v-for="share in getShares" :key="share.name"
                             @click="selectLogo(share)">
                             <img :src="share.path" :alt="share.name">
                         </div>
-                        <div class="link-type" @click="selectLogo(getLogoByName('Autre'))">
+                        <div class="link-type img-container" @click="selectLogo(getLogoByName('Autre'))">
                             <img src="@/assets/img/logos/plus.png" alt="lien">
                         </div>
                     </div>
@@ -21,10 +21,24 @@
                 </div>
             </div>
         </modal>
-        <div class="shares-item" v-for="share in sharedLinks" :key="share.url">
-            <img :src="share.path" :alt="share.name">
+        <modal v-if="isDeleteModalOpen" title="Supprimer un lien" submit-label="Confirmer" cancel-label="Annuler" :with-submit="true"
+                @modal-submit="deleteShare" @modal-cancel="closeDeleteModal" >
+                <div class="delete-modale-root">
+                    <div class="modal-section">
+                        <div class="modal-section-form">Voulez-vous supprimer le lien suivant ?</div>
+                        <div class="modal-section-content modal-delete-container">
+                            <div class="modal-delete-item img-container" >
+                                <img :src="currentShareToDelete.path" :alt="currentShareToDelete.name">
+                            </div>
+                            <div class="modal-delete-item">{{currentShareToDelete.url}}</div>
+                        </div>
+                    </div>
+                </div>
+        </modal>
+        <div class="shares-item img-container" v-for="share in sharedLinks" :key="share.url">
+            <img :src="share.path" :alt="share.name" @click="openDeleteModal(share)">
         </div>
-        <div class="shares-item" @click="openModal">
+        <div class="shares-item img-container" @click="openAddModal">
             <img :src="getLogoByName('Plus').path" :alt="getLogoByName('Plus').name">
         </div>
     </div>
@@ -40,6 +54,10 @@ export default {
             isAddModalOpen : false,
             currentLogoSelected : {},
             inputLink : '',
+
+            isDeleteModalOpen : false,
+            currentShareToDelete : {},
+
             sharedLinks : []
         }
     },
@@ -72,17 +90,31 @@ export default {
                     })
                 }
             }
-            this.closeModal()
+            this.closeAddModal()
             console.log(this.sharedLinks)
         },
-        closeModal : function () {
+        deleteShare : function () {
+            this.sharedLinks = this.sharedLinks.filter(item => item.url !== this.currentShareToDelete.url)
+            this.closeDeleteModal()
+            console.log(this.sharedLinks)
+        },
+        openAddModal : function () {
+            this.isAddModalOpen = true
+        },
+        closeAddModal : function () {
             this.isAddModalOpen = false
             this.currentLogoSelected = {}
             this.inputLink = ''
         },
-        openModal : function () {
-            this.isAddModalOpen = true
+        openDeleteModal : function (share) {
+            this.currentShareToDelete = share
+            this.isDeleteModalOpen = true
         },
+        closeDeleteModal : function () {
+            this.isDeleteModalOpen = false
+            this.currentShareToDelete = {}
+        },
+        
         selectLogo : function (logo) {
             this.currentLogoSelected = logo
         }
@@ -104,6 +136,14 @@ export default {
     padding-left: 16px;;
 }
 
+.modal-delete-container {
+    display: flex
+}
+
+.modal-delete-item {
+    align-self: center;
+}
+
 .with-min-hight {
     min-height: 80px;
 }
@@ -114,12 +154,10 @@ export default {
 
 .shares-item {
     flex-grow: 1;
-    padding: 5px;
 }
 
 .link-type {
     display: inline-block;
-    padding: 5px;
     opacity: 1;
 }
 
@@ -130,6 +168,10 @@ export default {
 img {
     /* width : 40px; */
     background-color: blueviolet
+}
+
+.img-container {
+    padding: 5px
 }
 
 </style>
