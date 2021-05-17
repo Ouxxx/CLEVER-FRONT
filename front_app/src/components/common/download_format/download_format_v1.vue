2c8d460c6281cf8b0380c5da514de43d783b8c66
GalactusLeMinus <workitnowu@gmail.com>
2021-05-17 16:48:16 +0200

    next

---
+++ front_app/src/components/common/download_format/download_format_v1.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    <div class="download-format-root">
+        <div class="file">
+            <label for="file">Importer une image de présentation: </label>
+            <input type="file" id="myFile" name="filename" accept="">
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
